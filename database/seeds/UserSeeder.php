a3c0413b5917f2cbbbc76fd6d7412d8398c7e686
Wayne Smallman <wayne.smallman@octane.uk.net>
2020-07-28 14:48:35 +0100

    A seeder class for the users.

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => "Wayne Smallman",
+            'email' => 'wayne.smallman@octane.uk.net',
+            'email_verified_at' => date("Y-m-d H:i:s"),
+            'password' => 'e6z+CU7/(]Xa8[8KWUneQ4',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+    }
+}
